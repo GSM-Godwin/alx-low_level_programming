cc2192b77ad8bf1026f0d17023c2fcb3e822cc36
GSM-Godwin <godwinaliu29@gmail.com>
2023-02-24 16:52:14 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,25 @@
 
 /**
  * print_most_numbers - print num except 2 and 4
- *
+ * Description: Prints the numbers excluding 2 and 4
  * return: void
  */
 
 void print_most_numbers(void)
-{
-	char c;
 
-	for (c ='0'; c <= '9'; c++)
+{
+	int x = 0;
+
+	for (; c <= 9; c++)
 	{
-		if(!(c == '2' || c == '4'))
-			_putchar(c);
+	if(c == 2 || c == 4)
+	{continue;
+	}
+	else
+	{
+	_putchar(c + '0');
 	}
 	_putchar('\n');
 }
+
+
